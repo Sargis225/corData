7f478f6cfe63a8a1d5b8d384345ee11541e7f2bf
Sargis <sargisske@mail.ru>
2021-01-29 14:16:26 +0400

    Initial Commit

---
+++ corData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  corData
+//
+//  Created by Saq on 1/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
